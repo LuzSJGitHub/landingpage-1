5b55061468e1448705b21b55c856e0e49a156f81
Luz2021 <33913286+LuzSJGitHub@users.noreply.github.com>
2021-10-22 02:05:19 +0000

    commit

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -82,6 +82,8 @@ class ProductosController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def producto_params
-      params.require(:producto).permit(:nombre, :categorium_id)
+      params.require(:producto).permit(:nombre, :categorium_id,
+      :descripcion,
+      :imagen) 
     end
 end
