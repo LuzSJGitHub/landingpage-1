095b4c4b3d53d3c1d4badd867b7651fb8bb4c886
Luz2021 <33913286+LuzSJGitHub@users.noreply.github.com>
2021-10-28 02:07:30 +0000

    Carrito del lado del cliente

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,79 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Categorium.create([{
+  nombre: "Ropa de Mujer",
+  imagen: "https://... .jpg"
+}, {
+  nombre: "Ropa de Hombre",
+  imagen: "https://... .jpg"
+}, {
+  nombre: "Ropa de niños - uniformes de colegio",
+  imagen: "https://... .jpg"
+}, {
+  nombre: "Uniformes corporativos",
+  imagen: "https://... .jpg"
+}])
+
+Producto.create([{
+  nombre: "Blusas",
+  descripcion: "Aquí una descripción",
+  imagen: "https://... .jpg",
+  categorium_id: Categorium.first.id
+}, {
+  nombre: "Faldas",
+  descripcion: "Aquí una descripción",
+  imagen: "https://... .jpg",
+  categorium_id: Categorium.where(nombre: "Ropa de Mujer").first.id
+}, {
+  nombre: "Pantalón mujer",
+  descripcion: "Aquí una descripción",
+  imagen: "https://... .jpg",
+  categorium_id: Categorium.where(nombre: "Ropa de Mujer").first.id
+}, {
+  nombre: "Vestidos",
+  descripcion: "Aquí una descripción",
+  imagen: "https://... .jpg",
+  categorium_id: Categorium.where(nombre: "Ropa de Mujer").first.id
+}, {
+  nombre: "Camisas",
+  descripcion: "Aquí una descripción",
+  imagen: "https://... .jpg",
+  categorium_id: Categorium.where(nombre: "Ropa de Hombre").first.id
+}, {
+  nombre: "Pantalones",
+  descripcion: "Aquí una descripción",
+  imagen: "https://... .jpg",
+  categorium_id: Categorium.where(nombre: "Ropa de Hombre").first.id
+}, {
+  nombre: "Jumpers",
+  descripcion: "Aquí una descripción",
+  imagen: "https://... .jpg",
+  categorium_id: Categorium.where(nombre: "Ropa de niños - uniformes de colegio").first.id
+}, {
+  nombre: "Blusas niña",
+  descripcion: "Aquí una descripción",
+  imagen: "https://... .jpg",
+  categorium_id: Categorium.where(nombre: "Ropa de niños - uniformes de colegio").first.id
+}, {
+  nombre: "Camisas niño",
+  descripcion: "Aquí una descripción",
+  imagen: "https://... .jpg",
+  categorium_id: Categorium.where(nombre: "Ropa de niños - uniformes de colegio").first.id
+}, {
+  nombre: "Blusas corp",
+  descripcion: "Aquí una descripción",
+  imagen: "https://... .jpg",
+  categorium_id: Categorium.where(nombre: "Uniformes corporativos").first.id
+}, {
+  nombre: "Camisas corp",
+  descripcion: "Aquí una descripción",
+  imagen: "https://... .jpg",
+  categorium_id: Categorium.where(nombre: "Uniformes corporativos").first.id
+}, {
+  nombre: "Pantalones corp",
+  descripcion: "Aquí una descripción",
+  imagen: "https://... .jpg",
+  categorium_id: Categorium.where(nombre: "Uniformes corporativos").first.id
+}])
